dd41bd763deff894001ffa9c233d8c34a1ba94e6
Josh Clyde <joshclyde11@gmail.com>
2018-06-17 20:18:41 -0400

    add metronome visual

--- src/Metronome/Metronome.tsx
+++ src/Metronome/Metronome.tsx
@@ -13,7 +13,7 @@ class Metronome extends React.Component<IMetronomeProps, {timer: NodeJS.Timer}> 
     const { initialize, tick } = this.props;
     initialize();
     this.state = {
-      timer: setInterval(tick, 100),
+      timer: setInterval(tick, 30),
     };
   }
 
@@ -27,20 +27,17 @@ class Metronome extends React.Component<IMetronomeProps, {timer: NodeJS.Timer}> 
   
   render() {
     const { classes, bpm, previousTick, currentTick } = this.props;
-    if (currentTick - previousTick < 200) {
-      return (
-        <div>
-          {bpm}
-          {currentTick}
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          {bpm}
-        </div>
-      );
-    }
+    const showLength = 400;
+    // const opacity = Math.sqrt(Math.max((showLength - (currentTick - previousTick)) / showLength, 0));
+    const opacity = (showLength - (currentTick - previousTick)) / showLength;
+    const radius = Math.max(opacity * 25, 0);
+    return <div>
+      <svg height="50" width="50">
+        <circle cx="25" cy="25" r={radius} fill="#85EFC4" opacity={.3}/>
+        <circle cx="25" cy="25" r="15" fill="#85EFC4"/>
+        Sorry, your browser does not support inline SVG.
+      </svg>
+    </div>
   }
 }
 const styles = {
