6838ea0f68e10bffa4c52557f8b7b30e7492f76e
Josh Clyde <joshclyde11@gmail.com>
2018-06-17 21:40:20 -0400

    create TempoButton component in Metronome component

---
+++ src/Metronome/TempoButton.tsx/TempoButton.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import injectSheet from "react-jss";
+import { ITempoButtonProps } from "./types";
+
+const TempoButton = ({
+  alpha,
+  classes,
+}: ITempoButtonProps) => (
+  <div>
+    <svg height="50" width="50">
+      <circle className={`${classes.both} ${classes.back}`}/>
+      <circle className={`${classes.both} ${classes.front}`}/>
+      Sorry, your browser does not support inline SVG.
+    </svg>
+  </div>
+);
+
+const styles = {
+  both: {
+    cx: 25,
+    cy: 25,
+    fill: "#85EFC4",
+  },
+  front: {
+    r: 15,
+  },
+  back: {
+    r: props => Math.max(props.alpha * 25, 0),
+    opacity: .3,
+  },
+};
+
+// export default SmartTempoButton;
+export default injectSheet(styles)(TempoButton);
